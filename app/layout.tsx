8a273f40fda27209b33e12e8bdf121e9302925b3
ortbri <brian.ort02@icloud.com>
2025-01-08 15:47:05 -0800

    fix:file changes rm src

---
+++ app/layout.tsx
@@ -0,0 +1,37 @@
+import type { Metadata } from "next";
+import { Geist } from "next/font/google";
+import "./globals.css";
+import Footer from "../pages/web/nav/footer";
+import Header from "../pages/web/nav/header";
+import { ThemeProvider } from "../provider/theme-provider";
+import { Toaster } from "sonner";
+import { TooltipProvider } from "../components/ui/tooltip";
+
+const geistSans = Geist({
+	variable: "--font-geist-sans",
+	subsets: ["latin"],
+});
+
+export const metadata: Metadata = {
+	title: "Drawlings",
+	description: "2D design drawings for architecture students",
+};
+
+export default function RootLayout({
+	children,
+}: Readonly<{
+	children: React.ReactNode;
+}>) {
+	return (
+		<html lang="en" suppressHydrationWarning>
+			<body className={`${geistSans.variable} antialiased`}>
+				<ThemeProvider>
+					<Header />
+					<TooltipProvider>{children}</TooltipProvider>
+					<Toaster />
+					<Footer />
+				</ThemeProvider>
+			</body>
+		</html>
+	);
+}
---
+++ app/page.tsx
@@ -0,0 +1,21 @@
+import { ThemeToggle } from "../components/themeToggle";
+import { logout } from "./logout/actions";
+
+export default function Home() {
+	return (
+		<div className="flex h-screen flex-col items-center justify-center">
+			<a
+				href="/login"
+				className="rounded bg-blue-500 px-4 py-2 text-white transition-colors hover:bg-blue-600 active:bg-blue-700"
+			>
+				Testing route
+			</a>
+
+			<form action={logout}>
+				<button type="submit" className="bordered p-4">
+					logout
+				</button>
+			</form>
+		</div>
+	);
+}
