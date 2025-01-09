f47b9a4dea41d5182bd891e9d9f7fec28619c5ee
ortbri <brian.ort02@icloud.com>
2025-01-09 13:59:25 -0800

    testing precommit and pre push

--- app/(auth)/forgot-password/page.tsx
+++ app/(auth)/forgot-password/page.tsx
@@ -1,10 +1,10 @@
-import Image from "next/image"
-import Link from "next/link"
-import { Button } from "../../../components/ui/button"
-import { Input } from "../../../components/ui/input"
-import { Label } from "../../../components/ui/label"
-import { cn } from "../../../lib/utils"
-import ghost from "../../../public/ghost.png"
+import Image from 'next/image';
+import Link from 'next/link';
+import { Button } from '../../../components/ui/button';
+import { Input } from '../../../components/ui/input';
+import { Label } from '../../../components/ui/label';
+import { cn } from '../../../lib/utils';
+import ghost from '../../../public/ghost.png';
 
 export default function ForgotPasswordPage() {
   return (
@@ -30,12 +30,12 @@ export default function ForgotPasswordPage() {
         <div className="absolute inset-0 h-full w-full object-cover dark:brightness-[0.2] dark:grayscale" />
       </div>
     </div>
-  )
+  );
 }
 
 function ForgotPasswordForm() {
   return (
-    <form className={cn("flex flex-col gap-6")} action="/api/auth/forgot-password">
+    <form className={cn('flex flex-col gap-6')} action="/api/auth/forgot-password">
       <div className="flex flex-col items-center gap-2 text-center">
         <h1 className="text-2xl font-bold">Reset password</h1>
         <p className="text-balance text-sm text-muted-foreground">
@@ -45,23 +45,18 @@ function ForgotPasswordForm() {
       <div className="grid gap-6">
         <div className="grid gap-2">
           <Label htmlFor="email">Email</Label>
-          <Input
-            name="email"
-            type="email"
-            placeholder="m@example.com"
-            required
-          />
+          <Input name="email" type="email" placeholder="m@example.com" required />
         </div>
         <Button type="submit" className="w-full">
-          Send reset link
+          Send Link
         </Button>
       </div>
       <div className="text-center text-sm">
-        Remember your password?{" "}
+        Remember your password?{' '}
         <Link href="/login" className="underline underline-offset-4">
           Sign in
         </Link>
       </div>
     </form>
-  )
-} 
+  );
+}
