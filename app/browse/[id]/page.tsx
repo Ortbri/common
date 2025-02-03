47a8aea8f46dd512c4e7e9bc326dd2d1eaebceff
ortbri <brian.ort02@icloud.com>
2025-02-03 17:28:01 -0600

    fix:rm element id

--- app/browse/[id]/page.tsx
+++
@@ -1,9 +0,0 @@
-import { useRouter } from 'next/router';
-
-function ElementId() {
-  // item will use the slug
-  const router = useRouter();
-  return <div>elementId {router.query.slug}</div>;
-}
-
-export default ElementId;
