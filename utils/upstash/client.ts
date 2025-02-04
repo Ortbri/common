74bf9e5076dc5137e3c0b2f7b1f4b4c4cbfbeac7
ortbri <brian.ort02@icloud.com>
2025-02-04 10:43:41 -0600

    fix: env name change

--- utils/upstash/client.ts
+++ utils/upstash/client.ts
@@ -3,6 +3,6 @@ import { Redis } from '@upstash/redis';
 
 // Create and export the Redis client
 export const redis = new Redis({
-  url: process.env.UPSTASH_REDIS_URL, // Use the Redis URL from Upstash
-  token: process.env.UPSTASH_REDIS_TOKEN, // Use the Redis token from Upstash
+  url: process.env.UPSTASH_REDIS_REST_URL, // Use the Redis URL from Upstash
+  token: process.env.UPSTASH_REDIS_REST_TOKEN, // Use the Redis token from Upstash
 });
