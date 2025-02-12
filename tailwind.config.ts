5774bfc4ef50d642157dde095f2317b7165fa8ea
ortbri <brian.ort02@icloud.com>
2025-02-12 15:36:00 -0600

    fix:type issue with tailwind config.ts

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,6 +1,7 @@
 import svgToDataUri from 'mini-svg-data-uri'; // Correct import
 import type { Config } from 'tailwindcss';
 import { default as animate } from 'tailwindcss-animate';
+import plugin from 'tailwindcss/plugin';
 
 export default {
   darkMode: ['class'],
@@ -104,20 +105,20 @@ export default {
   },
   plugins: [
     animate,
-    function ({ matchUtilities, theme }) {
+    plugin(function({ matchUtilities, theme }) {
       matchUtilities(
         {
-          "bg-grid": (value) => ({
+          "bg-grid": (value: string) => ({
             backgroundImage: `url("${svgToDataUri(
               `<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 32 32" width="32" height="32" fill="none" stroke="${value}"><path d="M0 .5H31.5V32"/></svg>`
             )}")`,
           }),
-          "bg-grid-small": (value: any) => ({
+          "bg-grid-small": (value: string) => ({
             backgroundImage: `url("${svgToDataUri(
               `<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 32 32" width="8" height="8" fill="none" stroke="${value}"><path d="M0 .5H31.5V32"/></svg>`
             )}")`,
           }),
-          "bg-dot": (value: any) => ({
+          "bg-dot": (value: string) => ({
             backgroundImage: `url("${svgToDataUri(
               `<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 32 32" width="16" height="16" fill="none"><circle fill="${value}" id="pattern-circle" cx="10" cy="10" r="1.6257413380501518"></circle></svg>`
             )}")`,
@@ -125,6 +126,6 @@ export default {
         },
         { values: theme("colors"), type: "color" }
       );
-    },
+    }),
   ],
 } satisfies Config;
