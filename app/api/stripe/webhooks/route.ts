ac20b1629539f8a2134f41c2d323dc447f16b0da
ortbri <brian.ort02@icloud.com>
2025-02-12 08:09:12 -0600

    build:type file for stripe cache

--- app/api/stripe/webhooks/route.ts
+++ app/api/stripe/webhooks/route.ts
@@ -125,20 +125,3 @@ export async function syncStripeDataToKV(customerId: string) {
   return subData;
 }
 
-/* ---------------------------------- type ---------------------------------- */
-export type STRIPE_SUB_CACHE =
-  | {
-      subscriptionId: string | null;
-      status: Stripe.Subscription.Status;
-      priceId: string | null;
-      currentPeriodStart: number | null;
-      currentPeriodEnd: number | null;
-      cancelAtPeriodEnd: boolean;
-      paymentMethod: {
-        brand: string | null; // e.g., "visa", "mastercard"
-        last4: string | null; // e.g., "4242"
-      } | null;
-    }
-  | {
-      status: 'none';
-    };
---
+++ types/stripe.ts
@@ -0,0 +1,19 @@
+import Stripe from "stripe";
+
+/* ---------------------------------- type ---------------------------------- */
+export type STRIPE_SUB_KV_CACHE =
+  | {
+      subscriptionId: string | null;
+      status: Stripe.Subscription.Status;
+      priceId: string | null;
+      currentPeriodStart: number | null;
+      currentPeriodEnd: number | null;
+      cancelAtPeriodEnd: boolean;
+      paymentMethod: {
+        brand: string | null; // e.g., "visa", "mastercard"
+        last4: string | null; // e.g., "4242"
+      } | null;
+    }
+  | {
+      status: 'none';
+    };
